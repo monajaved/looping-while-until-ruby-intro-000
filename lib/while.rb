6c14fa97db70815a9bf4abce7c323712eb3d0ce0
Mona Javed Syed <monajaved93@gmail.com>
2020-10-17 18:11:49 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,10 +4,5 @@ def using_while
     levitation_force += 1
     puts "Wingardium Leviosa"
 
-  #your code here
-#   number = 0
-# while number <= 9
-#   number += 1
-#   puts "hi #{number}"
-# end
+
 end
